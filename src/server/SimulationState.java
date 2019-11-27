05ca9c23de7e2f5a1e650f66d97269f46f9af50f
joelh273 <45686194+joelh273@users.noreply.github.com>
2019-11-27 16:00:15 +0100

    x

--- src/server/SimulationState.java
+++ src/server/SimulationState.java
@@ -11,59 +11,55 @@ public class SimulationState implements Runnable {
 	SimulationState(Server server) {
 		this.server = server;
 	}
-	
-	private void movePlanes() throws RemoteException
-	{
-		for (int i=0;i<server.getModel().getGroundPlanes().size();i++)
-		{
+
+	private void movePlanes() throws RemoteException {
+		for (int i = 0; i < server.getModel().getGroundPlanes().size(); i++) {
 			server.getModel().getGroundPlanes().get(i).movePlane();
 		}
-		for (int i=0;i<server.getClients().size();i++)
-		{
+		for (int i = 0; i < server.getClients().size(); i++) {
 			server.getGroundPlanesDTO(server.getClients().get(i));
 		}
 	}
-	
-	private void checkCollision() throws RemoteException
-	{
-		for(int i=0;i<server.getModel().getGroundPlanes().size();i++)
-		{
-			for (int j = i+1; j < server.getModel().getGroundPlanes().size(); j++)
-			{
-				if(server.getModel().getGroundPlanes().get(i).getPosition().equals(server.getModel().getGroundPlanes().get(j).getPosition()))
-				{
-					for(int x=0;x<server.getClients().size();i++)
-					{
-						server.simulationFailed(server.getClients().get(x));
-					}
+
+	private boolean checkCollision() throws RemoteException {
+		for (int i = 0; i < server.getModel().getGroundPlanes().size(); i++) {
+			for (int j = i + 1; j < server.getModel().getGroundPlanes().size(); j++) {
+				if (server.getModel().getGroundPlanes().get(i).getPosition()
+						.equals(server.getModel().getGroundPlanes().get(j).getPosition())) {
+					return true;
 				}
 			}
 		}
+		return false;
 	}
 
 	@Override
 	public void run() {
 		System.out.println("SimulationState started");
-		while(true)
-		{
-		if( server.getModel().getGroundPlanes().size()>=1)
-		{
-		try {
-			movePlanes();
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		while (true) {
+			if (server.getModel().getGroundPlanes().size() >= 1) {
+				try {
+					movePlanes();
+				} catch (RemoteException e) {
+					e.printStackTrace();
+				}
+			}
+			if (server.getModel().getGroundPlanes().size() >= 2) {
+				try {
+					if (checkCollision()) {
+						break;
+					}
+				} catch (RemoteException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-		}
-		if(server.getModel().getGroundPlanes().size()>=2)
-		{
-		try {
-			checkCollision();
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		}
+		for (int x = 0; x < server.getClients().size(); x++) {
+			try {
+				server.simulationFailed(server.getClients().get(x));
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
